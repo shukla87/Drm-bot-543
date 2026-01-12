cb2302f66f47c2bd60aa6b067d1497b7b8fab6c8
shukla87 <shukladivyansh079@gmail.com>
2026-01-12 19:39:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1069,21 +1069,21 @@ async def txt_handler(bot: Client, m: Message):
 
             elif "https://cpvod.testbook.com/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://cp-api-sigma.vercel.app/sign?url={video_url}"
+                url = f"https://cp-api-sigma.vercel.app/sign?url={url}"
                 #url = f"https://scammer-keys.vercel.app/api?url={url}&token={cptoken}&auth=@scammer_botxz1"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp.com/drm/" in url:
-                url = f"https://cp-api-sigma.vercel.app/sign?url={video_url}"
+                url = f"https://cp-api-sigma.vercel.app/sign?url={url}"
                 #url = f"https://scammer-keys.vercel.app/api?url={url}&token={cptoken}&auth=@scammer_botxz1"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp" in url:
-                signed_api = f"https://cp-api-sigma.vercel.app/sign?url={video_url}"
+                signed_api = f"https://cp-api-sigma.vercel.app/sign?url={url}"
                 response = requests.get(signed_api, timeout=10)
                 url = response.text.strip()
                 
@@ -1412,13 +1412,13 @@ async def text_handler(bot: Client, m: Message):
 
             elif "https://cpvod.testbook.com/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = 'https://cp-api-sigma.vercel.app/sign?url={video_url}' + url
+                url = 'https://cp-api-sigma.vercel.app/sign?url={url}' + url
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp.com/drm/" in url:
-                url = 'https://cp-api-sigma.vercel.app/sign?url={video_url}' + url
+                url = 'https://cp-api-sigma.vercel.app/sign?url={url}' + url
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
